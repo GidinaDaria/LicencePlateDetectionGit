8d61b483773e1fd1dce4bf1ce8e3fda655e93522
Pavel Kraskovskiy <kraskovskiy94@gmail.com>
2015-09-05 15:45:42 +0300

    Публикация проекта

---
+++ LicencePlateDetection/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace LicencePlateDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 538);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Black;
+            this.trackBar1.Location = new System.Drawing.Point(618, 12);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(154, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 128;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            this.trackBar1.Leave += new System.EventHandler(this.trackBar1_Leave);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(618, 63);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Minimum = -255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(154, 45);
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            this.trackBar2.Leave += new System.EventHandler(this.trackBar2_Leave);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(618, 114);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(154, 45);
+            this.trackBar3.TabIndex = 4;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar3.ValueChanged += new System.EventHandler(this.trackBar3_ValueChanged);
+            this.trackBar3.Leave += new System.EventHandler(this.trackBar3_Leave);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(618, 165);
+            this.trackBar4.Maximum = 255;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(154, 45);
+            this.trackBar4.TabIndex = 5;
+            this.trackBar4.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar4.ValueChanged += new System.EventHandler(this.trackBar4_ValueChanged);
+            this.trackBar4.Leave += new System.EventHandler(this.trackBar4_Leave);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(618, 216);
+            this.trackBar5.Maximum = 255;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(154, 45);
+            this.trackBar5.TabIndex = 6;
+            this.trackBar5.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar5.ValueChanged += new System.EventHandler(this.trackBar5_ValueChanged);
+            this.trackBar5.Leave += new System.EventHandler(this.trackBar5_Leave);
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.Location = new System.Drawing.Point(618, 267);
+            this.trackBar6.Maximum = 100;
+            this.trackBar6.Minimum = -100;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(154, 45);
+            this.trackBar6.TabIndex = 8;
+            this.trackBar6.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar6.ValueChanged += new System.EventHandler(this.trackBar6_ValueChanged);
+            this.trackBar6.Leave += new System.EventHandler(this.trackBar6_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.trackBar6);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Распознавание автомобильных номеров";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar6;
+    }
+}
+
---
+++ LicencePlateDetection/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace LicencePlateDetection
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap Bitmap { get; set; }
+        private bool Applied { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private unsafe void Binarization(int threshold)
+        {
+            GC.Collect();
+            Bitmap bitmap = new Bitmap(Bitmap);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+            byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+            Parallel.For(0, heightInPixels, i =>
+            {
+                byte* currentLine = ptrFirstPixel + (i * bitmapData.Stride);
+                for (int j = 0; j < widthInBytes; j = j + bytesPerPixel)
+                {
+                    int brightness = (currentLine[j] + currentLine[j + 1] + currentLine[j + 2]) / 3;
+                    if (brightness <= threshold)
+                    {
+                        currentLine[j] = 0;
+                        currentLine[j + 1] = 0;
+                        currentLine[j + 2] = 0;
+                    }
+                    else
+                    {
+                        currentLine[j] = 255;
+                        currentLine[j + 1] = 255;
+                        currentLine[j + 2] = 255;
+                    }
+                }
+            });
+            bitmap.UnlockBits(bitmapData);
+            pictureBox1.Image = bitmap;
+        }
+
+        private unsafe void Contrast(int value)
+        {
+            GC.Collect();
+            double newValue = (100.0 + value) / 100.0;
+            newValue *= newValue;
+            Bitmap bitmap = new Bitmap(Bitmap);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+            byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+            Parallel.For(0, heightInPixels, i =>
+            {
+                byte* currentLine = ptrFirstPixel + (i * bitmapData.Stride);
+                for (int j = 0; j < widthInBytes; j = j + bytesPerPixel)
+                {
+                    double bValue = ((((currentLine[j] / 255.0) - 0.5) * newValue) + 0.5) * 255.0;
+                    double gValue = ((((currentLine[j + 1] / 255.0) - 0.5) * newValue) + 0.5) * 255.0;
+                    double rValue = ((((currentLine[j + 2] / 255.0) - 0.5) * newValue) + 0.5) * 255.0;
+                    currentLine[j] = Convert.ToByte((bValue < 0 ? 0 : (bValue > 255 ? 255 : bValue)));
+                    currentLine[j + 1] = Convert.ToByte((gValue < 0 ? 0 : (gValue > 255 ? 255 : gValue)));
+                    currentLine[j + 2] = Convert.ToByte((rValue < 0 ? 0 : (rValue > 255 ? 255 : rValue)));
+                }
+            });
+            bitmap.UnlockBits(bitmapData);
+            pictureBox1.Image = bitmap;
+        }
+
+        private unsafe void Brightness(int value)
+        {
+            GC.Collect();
+            Bitmap bitmap = new Bitmap(Bitmap);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+            byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+            Parallel.For(0, heightInPixels, i =>
+            {
+                byte* currentLine = ptrFirstPixel + (i * bitmapData.Stride);
+                for (int j = 0; j < widthInBytes; j = j + bytesPerPixel)
+                {
+                    int bValue = currentLine[j] + value;
+                    int gValue = currentLine[j + 1] + value;
+                    int rValue = currentLine[j + 2] + value;
+                    currentLine[j] = Convert.ToByte((bValue < 0 ? 0 : (bValue > 255 ? 255 : bValue)));
+                    currentLine[j + 1] = Convert.ToByte((gValue < 0 ? 0 : (gValue > 255 ? 255 : gValue)));
+                    currentLine[j + 2] = Convert.ToByte((rValue < 0 ? 0 : (rValue > 255 ? 255 : rValue)));
+                }
+            });           
+            bitmap.UnlockBits(bitmapData);
+            pictureBox1.Image = bitmap;
+        }
+
+        private unsafe void BlueFiltering(int value)
+        {
+            GC.Collect();
+            Bitmap bitmap = new Bitmap(Bitmap);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+            byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+            Parallel.For(0, heightInPixels, i =>
+            {
+                byte* currentLine = ptrFirstPixel + (i * bitmapData.Stride);
+                for (int j = 0; j < widthInBytes; j = j + bytesPerPixel)
+                {
+                    int gValue = currentLine[j + 1] - value;
+                    int rValue = currentLine[j + 2] - value;
+                    currentLine[j + 1] = Convert.ToByte(gValue < 0 ? 0 : gValue);
+                    currentLine[j + 2] = Convert.ToByte(rValue < 0 ? 0 : rValue);
+                }
+            });
+            bitmap.UnlockBits(bitmapData);
+            pictureBox1.Image = bitmap;
+        }
+
+        private unsafe void GreenFiltering(int value)
+        {
+            GC.Collect();
+            Bitmap bitmap = new Bitmap(Bitmap);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+            byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+            Parallel.For(0, heightInPixels, i =>
+            {
+                byte* currentLine = ptrFirstPixel + (i * bitmapData.Stride);
+                for (int j = 0; j < widthInBytes; j = j + bytesPerPixel)
+                {
+                    int bValue = currentLine[j] - value;
+                    int rValue = currentLine[j + 2] - value;
+                    currentLine[j] = Convert.ToByte(bValue < 0 ? 0 : bValue);
+                    currentLine[j + 2] = Convert.ToByte(rValue < 0 ? 0 : rValue);
+                }
+            });
+            bitmap.UnlockBits(bitmapData);
+            pictureBox1.Image = bitmap;
+        }
+
+        private unsafe void RedFiltering(int value)
+        {
+            GC.Collect();
+            Bitmap bitmap = new Bitmap(Bitmap);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(bitmap.PixelFormat) / 8;
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+            byte* ptrFirstPixel = (byte*)bitmapData.Scan0;
+            Parallel.For(0, heightInPixels, i =>
+            {
+                byte* currentLine = ptrFirstPixel + (i * bitmapData.Stride);
+                for (int j = 0; j < widthInBytes; j = j + bytesPerPixel)
+                {
+                    int bValue = currentLine[j] - value;
+                    int gValue = currentLine[j + 1] - value;
+                    currentLine[j] = Convert.ToByte(bValue < 0 ? 0 : bValue);
+                    currentLine[j + 1] = Convert.ToByte(gValue < 0 ? 0 : gValue);
+                }
+            });
+            bitmap.UnlockBits(bitmapData);
+            pictureBox1.Image = bitmap;
+        }
+
+        /*private void MedianFilter()
+        {
+            Bitmap bitmap = new Bitmap(Bitmap);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            IntPtr ptr = bitmapData.Scan0;
+            int bytes = Math.Abs(bitmapData.Stride) * bitmap.Height;
+            byte[] rgbValues = new byte[bytes];
+            Marshal.Copy(ptr, rgbValues, 0, bytes);
+            float rgb = 0;
+            for (int i = 3; i < rgbValues.Length; i += 4)
+            {
+                //rgb = rgbValues[i]*0.3f
+                //rgb += rgbValues[i - 1]
+                int brightness = (rgbValues[i - 3] + rgbValues[i - 2] + rgbValues[i - 1]) / 3;
+                if (brightness <= 0)
+                {
+                    rgbValues[i - 3] = 0;
+                    rgbValues[i - 2] = 0;
+                    rgbValues[i - 1] = 0;
+                }
+                else
+                {
+                    rgbValues[i - 3] = 255;
+                    rgbValues[i - 2] = 255;
+                    rgbValues[i - 1] = 255;
+                }
+            }
+            Marshal.Copy(rgbValues, 0, ptr, bytes);
+            bitmap.UnlockBits(bitmapData);
+            pictureBox1.Image = bitmap;
+
+        }*/
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap = new Bitmap(pictureBox1.Image);
+            Applied = false;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            if (!Applied)
+            {
+                Binarization(trackBar1.Value);
+            }
+            else
+            {
+                Applied = false;
+            }
+        }
+
+        private void trackBar2_ValueChanged(object sender, EventArgs e)
+        {
+            if (!Applied)
+            {
+                Brightness(trackBar2.Value);
+            }
+            else
+            {
+                Applied = false;
+            }
+        }
+
+        private void trackBar3_ValueChanged(object sender, EventArgs e)
+        {
+            if (!Applied)
+            {
+                BlueFiltering(trackBar3.Value);
+            }
+            else
+            {
+                Applied = false;
+            }
+        }
+
+        private void trackBar4_ValueChanged(object sender, EventArgs e)
+        {
+            if (!Applied)
+            {
+                GreenFiltering(trackBar4.Value);
+            }
+            else
+            {
+                Applied = false;
+            }
+        }
+
+        private void trackBar5_ValueChanged(object sender, EventArgs e)
+        {
+            if (!Applied)
+            {
+                RedFiltering(trackBar5.Value);
+            }
+            else
+            {
+                Applied = false;
+            }
+        }
+
+        private void trackBar6_ValueChanged(object sender, EventArgs e)
+        {
+            if (!Applied)
+            {
+                Contrast(trackBar6.Value);
+            }
+            else
+            {
+                Applied = false;
+            }
+        }
+
+        private void trackBar1_Leave(object sender, EventArgs e)
+        {
+            trackBar1.Value = 128;
+        }
+
+        private void trackBar2_Leave(object sender, EventArgs e)
+        {
+            trackBar2.Value = 0;
+        }
+
+        private void trackBar3_Leave(object sender, EventArgs e)
+        {
+            trackBar3.Value = 0;
+        }
+
+        private void trackBar4_Leave(object sender, EventArgs e)
+        {
+            trackBar4.Value = 0;
+        }
+
+        private void trackBar5_Leave(object sender, EventArgs e)
+        {
+            trackBar5.Value = 0;
+        }
+
+        private void trackBar6_Leave(object sender, EventArgs e)
+        {
+            trackBar6.Value = 0;
+        }      
+    }
+}
